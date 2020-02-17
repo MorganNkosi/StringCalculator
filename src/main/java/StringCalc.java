0576a1b01015123e307794db9b286ff967a62e21
MorganNkosi <nkosizmorgan@hotmail.com>
2020-02-17 17:47:03 +0200

    Mon17

--- src/main/java/StringCalc.java
+++ src/main/java/StringCalc.java
@@ -1,26 +1,40 @@
 public class StringCalc
 {
-        public static int Add(String cin) throws Exception {
+    public static void main(String[] args) throws Exception {
+       try {
+           System.out.println(Add("1;2,1000,2"));
+       }catch (Exception e)
+       {
+           System.out.println(e.getMessage());
+       }
+    }
+
+    public static int Add(String cin) throws Exception {
+       String delimiter = ";|,|\n";
        if(cin.startsWith("//"))
        {
-            cin = cin.replace("4",";");
+//            cin = cin.replace("4",";");
             int index = cin.indexOf("\n");
             cin = cin.substring(index+1);
+            delimiter += cin.substring(2,index-1);
+
        }
-        String[] numbers = cin.split(";|,|\n");
+        String[] numbers = cin.split(delimiter);
         int sum = 0;
+        StringBuilder negative = new StringBuilder();
         if (!cin.isEmpty())
         {
             for (int i = 0; i < numbers.length; i++)
             {
-                if (Integer.parseInt(numbers[i]) < 0 )
+                if (Integer.parseInt(numbers[i]) < 0)
                 {
-                    throw new Exception("Negative number");
+                    negative.append(Integer.parseInt(numbers[i])).append(",");
+                    throw new Exception("ERROR: negatives not allowed " + negative);
                 }
-
-            }
-            for (int i = 0; i < numbers.length; i++)
-            {
+                if (Integer.parseInt(numbers[i]) > 999)
+                {
+                    i++;
+                }
                 sum += Integer.parseInt(numbers[i]);
             }
         }
--- src/test/java/StringCalcTest.java
+++ src/test/java/StringCalcTest.java
@@ -14,12 +14,53 @@ class StringCalcTest {
     }
 
     @Test
-    void add() throws Exception {
+    void addTwoIntegers() throws Exception {
+        assertEquals(StringCalc.Add(""),0);
+        assertEquals(StringCalc.Add("1"),1);
+        assertEquals(StringCalc.Add("1,1"),2);
+    }
+
+    @Test
+    void addMultipleIntegers() throws Exception {
+        assertEquals(StringCalc.Add("1,2,3,4"),10);
+        assertEquals(StringCalc.Add(""),0);
+        assertEquals(StringCalc.Add("1"),1);
+        assertEquals(StringCalc.Add("1,1"),2);
+    }
+
+    @Test
+    void addWithNewLine() throws Exception {
+        assertEquals(StringCalc.Add("1\n2,3"),6);
+    }
+
+    @Test
+    void addWithDiffDelimiters() throws Exception {
+        assertEquals(StringCalc.Add("//;\n1;2"),3);
         assertEquals(StringCalc.Add("//4\n142"),3);
     }
 
     @Test
-    void except() throws Exception {
-        StringCalc.Add("-1");
+    void handleNegative() throws Exception {
+        assertThrows(Exception.class, ()-> StringCalc.Add("-3;5"));
+    }
+
+    @Test
+    void ignoreGreaterThan1000() throws Exception {
+        assertEquals(StringCalc.Add("//;\n1000,1;2"),3);
+    }
+
+    @Test
+    void anyLengthDelimiter() throws Exception {
+        assertEquals(StringCalc.Add("//4\n142"),3);
+    }
+
+    @Test
+    void diffDelimiterAnyLength() throws Exception {
+        assertEquals(StringCalc.Add("//4\n142"),3);
+    }
+
+    @Test
+    void invalidInput() throws Exception {
+        assertEquals(StringCalc.Add("//4\n142"),3);
     }
 }
